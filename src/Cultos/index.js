e5d63d6bc9e7901bb97601fae60b5139965fe6ab
Gabriel <gf4506799@gmail.com>
2026-02-11 20:08:28 -0300

    Cultos adicionados mas precisam de edição - tentativa de adicionar ícone

--- src/Cultos/index.js
+++ src/Cultos/index.js
@@ -1,12 +1,41 @@
 import React from "react"
 import { Header } from "../Components/header"
-import { Conteiner } from "./styles"
+import { Conteiner, CardItemsCulto, Div } from "./styles"
 
 export function Cultos() {
+  const cultos = [
+    { id: 1, cultoNome: "Culto de Oração", horario: "Domingo 08:00 da manhã" },
+    {
+      id: 2,
+      cultoNome: "Culto Vida no Espírito",
+      horario: "Domingo 09:30 da manhã",
+    },
+    { id: 3, cultoNome: "Culto de Celebração", horario: "Domingo 18:30" },
+    {
+      id: 4,
+      cultoNome: "Culto Quinta da Resposta",
+      horario: "5ª Feira ás 19:30",
+    },
+  ]
+
   return (
     <Conteiner>
       <Header />
-      <h1>Cultos</h1>
+      <h1>Programação semanal de Cultos</h1>
+      <Div>
+
+      <ul>
+        {cultos.map((cultos) => (
+          <CardItemsCulto>
+
+            <li key={cultos.id}>
+              <p>{cultos.cultoNome}</p> <p>{cultos.horario}</p>
+            </li>
+            
+          </CardItemsCulto>
+        ))}
+      </ul>
+        </Div>
     </Conteiner>
   )
 }
--- src/Cultos/styles.js
+++ src/Cultos/styles.js
@@ -4,4 +4,35 @@ export const Conteiner = styled.div`
   background-color: #C2C5AA;
   height: 100vh;
   width: 100vw;
+
+  h1{
+    text-align: center;
+    margin-top: 10px;
+    color: #ffd900;
+    text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);
+    
+  }
+`
+export const Div = styled.div`
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 95px;
+
+  ul{
+    list-style-type: ""
+  }
+`
+
+export const CardItemsCulto = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: #ffff;
+  margin: 10px;
+  display: inline-flex;
+  border-radius: 8px;
+  justify-content: center;
+  align-items: center;
+  
 `
